c5d206f7017fb5077a56dc5e25dd31e0c2386daa
psachin <iclcoolster@gmail.com>
2014-04-30 11:51:24 +0530

    Subject: Tested ft5x, gt811, ektf2k & gslx1680 touch controllers.
    Description:

--- probe_tc.sh
+++ probe_tc.sh
@@ -4,6 +4,8 @@
 
 TC_IN_SCRIPT_DOT_BIN=$(cat /tmp/script.fex | grep "ctp_used = 1" -A1 | grep ctp_name | sed 's/.*"\(.*\)".*/\1/g')
 
+TC_ADDR_IN_SCRIPT_DOT_BIN=$(cat /tmp/script.fex | grep "ctp_used = 1" -A3 | grep ctp_twi_addr | cut -d "x" -f 2)
+
 echo $TC_IN_SCRIPT_DOT_BIN 
 
 
@@ -73,19 +75,25 @@ function ektf2k() {
 
 
 function gslx1680() {
+    # Load the driver
+    insmod /lib/modules/3.4.75+/kernel/drivers/input/touchscreen/gslx680_ts.ko
     lsmod | grep "gslx680_ts"
+    
     if [ "$(echo $?)" -eq 0 ];
     then
-	echo "$(date): ${TC_IN_SCRIPT_DOT_BIN} module already loaded" >> /var/log/aakash_tc.log
+	echo "$(date): gslx1680 module already loaded" >> /var/log/aakash_tc.log
     else
 	cp -v /home/aakash/mount/script.bin.gslx1680 /home/aakash/mount/script.bin
 	cp -v /lib/modules/uImage.3.4.75 /home/aakash/mount/uImage
-	echo -e "8192cu\nrtl8192cu\nrtlwifi" > /etc/modules
+	echo -e "8192cu\nrtl8192cu\nrtlwifi\n#gslx680_ts" > /etc/modules
 
 	echo "$(date): Deleting xorg.conf.d" >> /var/log/aakash_tc.log
 	rm -r /usr/share/X11/xorg.conf.d
 	echo "$(date): Copying xorg.conf.d.orig to xorg.conf.d" >> /var/log/aakash_tc.log
 	cp -r /usr/share/X11/xorg.conf.d.orig /usr/share/X11/xorg.conf.d
+
+	# Load the driver
+	# insmod /lib/modules/3.4.75+/kernel/drivers/input/touchscreen/gslx680_ts.ko
 
 	wait
 	reboot
@@ -94,8 +102,15 @@ function gslx1680() {
 
 if [ ${TC_IN_SCRIPT_DOT_BIN} == "ft5x_ts" ];
 then
-    echo "$(date): Detected ft5x" >> /var/log/aakash_tc.log
-    ft5x
+    # 'gslx1680' in script.bin has ctp_name as 'ft5x_ts', but ctp_twi_addr is '0x3f'
+    if [ ${TC_ADDR_IN_SCRIPT_DOT_BIN} == "3f" ];
+    then
+	echo "$(date): Detected gslx1680" >> /var/log/aakash_tc.log
+	gslx1680
+    else			# Load ft5x_ts driver
+	echo "$(date): Detected ft5x" >> /var/log/aakash_tc.log
+	ft5x
+    fi
 elif [ ${TC_IN_SCRIPT_DOT_BIN} == "Goodix-TS" ];
 then 
     echo "$(date): Detected Goodix-TS" >> /var/log/aakash_tc.log
@@ -104,10 +119,6 @@ elif [ ${TC_IN_SCRIPT_DOT_BIN} == "elan_ts" ];
 then 
     echo "$(date): Detected ektf2k" >> /var/log/aakash_tc.log
     ektf2k
-elif [ ${TC_IN_SCRIPT_DOT_BIN} == "gslx1680" ];
-then 
-    echo "$(date): Detected gslx1680" >> /var/log/aakash_tc.log
-    gslx1680
 else
     echo "$(date): $TC_IN_SCRIPT_DOT_BIN" >> /var/log/aakash_tc.log
     echo "$(date): No suitable touch driver found." >> /var/log/aakash_tc.log
